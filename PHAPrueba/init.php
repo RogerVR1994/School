a31ba35c369c59c9d050658ef321c3e03cd3a2a2
Rogelio Ventura <rogeliovr94@gmail.com>
2017-02-17 07:56:18 -0600

    Ya funcionan p[aginas de registro

--- PHAPrueba/init.php
+++ PHAPrueba/init.php
@@ -1,3 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registro de Usuario</title>
+</head>
+<body>
+<form action="medadd.php" method="post">
+ <p>Nombre: <input type="text" name="nombre" /></p>
+ <p>Medicina 1: <input type="text" name="med1" /></p>
+ <p>Frecuencia: <input type="text" name="frec1" /></p>
+ <p>Medicina 2: <input type="text" name="med2" /></p>
+ <p>Frecuencia: <input type="text" name="frec2" /></p>
+ <p>Medicina 3: <input type="text" name="med3" /></p>
+ <p>Frecuencia: <input type="text" name="frec3" /></p>
+ <p>Medicina 4: <input type="text" name="med4" /></p>
+ <p>Frecuencia: <input type="text" name="frec4" /></p>
+ <p><input type="submit" /></p>
+</form>
+</body>
+</html>
 <?php 
 	echo "hola2";
 ?>
--- PHAPrueba/registro.php
+++ PHAPrueba/registro.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registo de Usuarios</title>
+</head>
+<body>
+<form action="medadd.php" method="post">
+ <p>Nombre: <input type="text" name="nombre" /></p>
+ <p>Apellido: <input type="text" name="med1" /></p>
+ <p>E-mail: <input type="email"></p>
+ <p><input type="submit" /></p>
+</form>
+</body>
+</html>
 <?php 
 	echo "hola";
 ?>
